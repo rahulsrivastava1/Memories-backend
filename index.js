b7793a8f2ea042581a02cda225491d62609edf87
Rahul Srivastava <srivastavar433@gmail.com>
2023-03-15 09:01:21 +0530

    database issue fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js";
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+dotenv.config();
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
@@ -18,8 +20,9 @@ app.get("/", (req, res) => {
   res.send("APP is Running.");
 });
 
-const CONNECTION_URL =
-  "mongodb+srv://rahul:Rahulraj@1@cluster0.jee9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+console.log(process.env.MONGO_DB_USERNAME);
+
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.jee9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose
